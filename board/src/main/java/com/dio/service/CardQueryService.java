95a1fb3ac7ab07254a594dd20b194dd79ffb6e85
Gabriela Oliveira <gabriela.gomestrab@gmail.com>
2025-08-22 10:34:42 -0300

    Camada Service

---
+++ board/src/main/java/com/dio/service/CardQueryService.java
@@ -0,0 +1,22 @@
+package com.dio.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import com.dio.dto.CardDetailsDTO;
+import com.dio.persistence.dao.CardDAO;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CardQueryService {
+
+    private final Connection connection;
+
+    public Optional<CardDetailsDTO> findById(final Long id) throws SQLException {
+        var dao = new CardDAO(connection);
+        return dao.findById(id);
+    }
+
+}
